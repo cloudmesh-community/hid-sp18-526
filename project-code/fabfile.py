6e316b531efca40bfee2c86ac24f26d0ec0ba326
Tim Whitson <whitstd@gmail.com>
2018-04-25 15:18:22 -0400

    update fabfile

--- project-code/fabfile.py
+++ project-code/fabfile.py
@@ -1,8 +1,11 @@
 import os
 
 import yaml
+import connexion
 from fabric.api import local
 
+from document import APIDoc
+from resolver import CMResolver
 import constants
 
 with open('config.' + constants.YAML_EXTENSION, 'r') as f:
@@ -32,4 +35,14 @@ def deploy():
     local('pip install ' + ' '.join(pip_packages))
     
 def run():
-    print('running ... jk')
+    doc = APIDoc(
+        title = 'yolo',
+        version = '1.0',
+        swagger = '2.0'
+    )
+
+    controller_dispatch = doc.set_apis(config['apis'])
+
+    app = connexion.App(__name__)
+    app.add_api(doc.to_dict(), resolver = CMResolver(controller_dispatch))
+    app.run(port = 8080)
