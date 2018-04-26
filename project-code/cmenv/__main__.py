994e3ffd5a0233482790b000d5545af7179cb114
Tim Whitson <whitstd@gmail.com>
2018-04-26 12:04:18 -0400

    add main and init

---
+++ project-code/cmenv/__main__.py
@@ -0,0 +1,19 @@
+import connexion
+import yaml
+from cmenv.document import APIDoc
+from cmenv.resolver import CMResolver
+import cmenv.constants as constants
+import cmenv.config as config
+
+def main():
+    doc = APIDoc(
+        title = 'yolo',
+        version = '1.0',
+        swagger = '2.0'
+    )
+
+    controller_dispatch = doc.set_apis(config.apis)
+
+    app = connexion.App(__name__)
+    app.add_api(doc.to_dict(), resolver = CMResolver(controller_dispatch))
+    app.run(port = 8080)
